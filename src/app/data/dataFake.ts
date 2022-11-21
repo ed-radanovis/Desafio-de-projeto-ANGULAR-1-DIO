45cf1321a805bb100e0ef9f622efd9e04ce2dc09
Edmar Radanovis <edradanovis@gmail.com>
2022-11-21 17:38:31 -0300

    Initial commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Artigo do card principal do awesome-blog',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    photoCover:
+      'https://impacttheory.com/wp-content/uploads/2018/11/placeholder.png',
+  },
+  {
+    id: '2',
+    title: 'Artigo do small card 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et ipsum massa. Nullam ultrices, libero ut iaculis cursus, sapien nunc congue ipsum, at venenatis justo arcu ut ante. Duis pellentesque tortor eu nulla blandit, id aliquam risus finibus. Donec suscipit quam rutrum velit volutpat, porttitor suscipit arcu iaculis. Praesent eros justo, faucibus non nisl id, congue elementum tortor. Sed posuere odio non ligula lacinia, sit amet bibendum leo interdum. Nam rutrum, nisi sed commodo fermentum, dolor enim pellentesque mauris, vitae feugiat ligula libero quis erat. In hac habitasse platea dictumst. Aliquam varius lorem sed tortor finibus consectetur. Donec a vestibulum sem. Nunc at erat fringilla, tempor quam gravida, interdum tortor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur molestie imperdiet ex, nec venenatis ligula dignissim et. Sed eget libero vel sapien faucibus dapibus.',
+    photoCover:
+      'https://impacttheory.com/wp-content/uploads/2018/11/placeholder.png',
+  },
+  {
+    id: '3',
+    title: 'Artigo do small card 2',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et ipsum massa. Nullam ultrices, libero ut iaculis cursus, sapien nunc congue ipsum, at venenatis justo arcu ut ante. Duis pellentesque tortor eu nulla blandit, id aliquam risus finibus. Donec suscipit quam rutrum velit volutpat, porttitor suscipit arcu iaculis. Praesent eros justo, faucibus non nisl id, congue elementum tortor. Sed posuere odio non ligula lacinia, sit amet bibendum leo interdum. Nam rutrum, nisi sed commodo fermentum, dolor enim pellentesque mauris, vitae feugiat ligula libero quis erat. In hac habitasse platea dictumst. Aliquam varius lorem sed tortor finibus consectetur. Donec a vestibulum sem. Nunc at erat fringilla, tempor quam gravida, interdum tortor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur molestie imperdiet ex, nec venenatis ligula dignissim et. Sed eget libero vel sapien faucibus dapibus.',
+    photoCover:
+      'https://impacttheory.com/wp-content/uploads/2018/11/placeholder.png',
+  },
+  {
+    id: '4',
+    title: 'Artigo do small card 3',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et ipsum massa. Nullam ultrices, libero ut iaculis cursus, sapien nunc congue ipsum, at venenatis justo arcu ut ante. Duis pellentesque tortor eu nulla blandit, id aliquam risus finibus. Donec suscipit quam rutrum velit volutpat, porttitor suscipit arcu iaculis. Praesent eros justo, faucibus non nisl id, congue elementum tortor. Sed posuere odio non ligula lacinia, sit amet bibendum leo interdum. Nam rutrum, nisi sed commodo fermentum, dolor enim pellentesque mauris, vitae feugiat ligula libero quis erat. In hac habitasse platea dictumst. Aliquam varius lorem sed tortor finibus consectetur. Donec a vestibulum sem. Nunc at erat fringilla, tempor quam gravida, interdum tortor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur molestie imperdiet ex, nec venenatis ligula dignissim et. Sed eget libero vel sapien faucibus dapibus.',
+    photoCover:
+      'https://impacttheory.com/wp-content/uploads/2018/11/placeholder.png',
+  },
+];
